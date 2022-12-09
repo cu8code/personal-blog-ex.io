ef9b46f55a33d553d44f6bc9618df92f249f70e8
cu8code <git8ankanroy@gmail.com>
2022-12-10 01:10:44 +0530

    a

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
 import style from "./Nav.module.css";
 
 export function Nav(props: { links: string[] }) {
   return (
     <nav className={style.gap}>
       {props.links.map((l) => (
-        <a className={`${style.gap} ${style.large}`} href={l}>
+        <a key={l}className={`${style.gap} ${style.large}`} href={l}>
           {l}
         </a>
       ))}
