85a0c1a06308130a494819dac4e137485cbeeb53
cu8code <git8ankanroy@gmail.com>
2022-12-17 00:55:14 +0530

    lol

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -5,10 +5,29 @@ type Links = Array<{
   href: string;
 }>;
 
-const apps: Links = [
+const good_piracy_webistes: Links = [
   {
-    txt: "Some",
-    href: "",
+    txt: "Anime",
+    href: "https://animixplay.to/",
+  },
+  {
+    txt: "Movie",
+    href: "https://vegamovies.day",
+  },
+  {
+    txt: "Movie2",
+    href: "https://hurawatch.at/",
+  },
+];
+
+const develeoper_stuff: Links = [
+  {
+    txt: "free API to use",
+    href: "https://github.com/public-apis/public-apis",
+  },
+  {
+    txt: "frequently used SVG",
+    href: "https://github.com/n3r4zzurr0/svg-spinners",
   },
 ];
 
@@ -17,19 +36,18 @@ export default function Links() {
     <Layout>
       <div>
         <h3># Friends</h3>
-        <ul>{toJSX(apps)}</ul>
+        <p>
+          its totaly not like i dont have friends i just dont want to scrifice
+          their privacy...
+        </p>
       </div>
       <div>
-        <h3># Apps</h3>
-        <ul>{toJSX(apps)}</ul>
+        <h3># Good Piracy Website </h3>
+        <ul>{toJSX(good_piracy_webistes)}</ul>
       </div>
       <div>
-        <h3># Cool websites</h3>
-        <ul>{toJSX(apps)}</ul>
-      </div>
-      <div>
-        <h3># Repository</h3>
-        <ul>{toJSX(apps)}</ul>
+        <h3># Develeoper Stuff </h3>
+        <ul>{toJSX(develeoper_stuff)}</ul>
       </div>
     </Layout>
   );
@@ -39,7 +57,7 @@ function toJSX(e: Links) {
   return e.map((item) => {
     return (
       <li key={item.txt}>
-        <a  href={item.href}>
+        <a rel="noreferrer" target={"_blank"} href={item.href}>
           {item.txt}
         </a>
       </li>
